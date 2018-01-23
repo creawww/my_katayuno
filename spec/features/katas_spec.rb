1afedf93a36c7ad0138f0ea2b11f322e37334530
Manuel Gomez <creawww@gmail.com>
2018-01-23 19:19:46 +0100

    second test katas

--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 require 'capybara'
 describe 'Kata' do
-  it 'can be read' do
-    kata_title = 'kata title'
-    kata_description = 'kata description'
 
-    kata= Kata.new(
-      title: kata_title,
-      description: kata_description
-    )
-    kata.save
+  it 'can be read' do
+    kata_title='Second title'
+    kata_description='Second Description'
+
+    create_kata(title:'title', description:'description')
+    create_kata(title:kata_title, description: kata_description)
 
     visit root_path
     click_on(kata_title)
 
     expect(page).to have_content(kata_title)
     expect(page).to have_content(kata_description)
+
+  end
+
+  def create_kata(title:'kata title', description:'description kata')
+    kata = Kata.new(title: title, description: description)
+    kata.save
 
   end
 end
